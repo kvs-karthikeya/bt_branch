1c78de245647cf2a87d5a22f96dd5e86563c937d
kvs-karthikeya <karthikeya.kvs2004@gmail.com>
2026-01-06 23:04:05 +0530

    Update logo image source in navigation component

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -34,7 +34,7 @@ export default function Navigation() {
           {/* Logo */}
           <Link href="/" className="flex items-center">
             <Image
-              src="/bt.jpeg" // put your image in /public/logo.png
+              src="/bt1.jpeg" // put your image in /public/logo.png
               alt="Logo"
               width={220}
               height={80}
